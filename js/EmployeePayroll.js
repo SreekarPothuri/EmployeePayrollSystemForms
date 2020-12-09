a17c9800fd580ca3236812ff4f1bb2ab525ee0e4
Your Name <you@example.com>
2020-12-09 11:19:15 +0530

    Modified: getters and setters of startDate

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -41,15 +41,13 @@ class EmployeePayrollData {
 
     get startDate() { return this._startDate; }
     set startDate(startDate) {
-            this._startDate = startDate;
+        this._startDate = startDate;    
     }
 
     //method
     toString() {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = !this.startDate ? "undefined" : this.startDate.toLocalDateString("en-US", options);
         return "id=" + this.id + ", name=" + this.name + ", gender=" + this.gender +
             ", profilePic=" + this.profilePic + ", department=" + this.department +
-            ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
+            ", salary=" + this.salary + ", startDate=" + this._startDate + ", note=" + this.note;
     }
 }
